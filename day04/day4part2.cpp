e2837a63652fc864b10e50fea04ebdc0616e88e7
Lauren Mandell <108501401+laurenmandell@users.noreply.github.com>
2022-12-03 22:18:53 -0800

    finish day 4

---
+++ day04/day4part2.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+int main(int argc, char** argv) {
+  std::ifstream file("day4.txt");
+  std::string line;
+  int count = 0;
+  while (getline(file, line)) {
+    std::string num1str, num2str, num3str, num4str;
+    std::stringstream ss(line);
+    std::getline(ss, num1str, '-');
+    std::getline(ss, num2str, ',');
+    std::getline(ss, num3str, '-');
+    std::getline(ss, num4str, '\n');
+
+    int num1 = std::stoi(num1str);
+    int num2 = std::stoi(num2str);
+    int num3 = std::stoi(num3str);
+    int num4 = std::stoi(num4str);
+
+    if (num3 >= num1 && num3 <= num2) {
+      count++;
+    } else if (num2 >= num3 && num2 <= num4) {
+      count++;
+    } else if (num1 <= num4 && num1 >= num3) {
+      count++;
+    } else if (num4 >= num1 && num4 <= num2) {
+      count++;
+    }
+  }
+  std::cout << count << "\n";
+}
